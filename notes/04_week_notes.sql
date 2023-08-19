ea3b396ac3924e1e0703d7d03951df11c3d5d817
Veneta <venkireva80@gmail.com>
2023-08-19 12:31:41 +0300

    Add week 4 - notes

---
+++ notes/04_week_notes.sql
@@ -0,0 +1,10 @@
+use movies;
+
+-- COALESCE returns the first non-null value from a list
+SELECT COALESCE(ms.NAME, me.NAME) AS NAME, ms.BIRTHDATE, me.NETWORTH
+FROM MOVIESTAR ms
+FULL JOIN MOVIEEXEC me ON ms.NAME = me.NAME;
+
+SELECT ms.NAME, me.NAME, ms.BIRTHDATE, me.NETWORTH
+FROM MOVIESTAR ms
+FULL JOIN MOVIEEXEC me ON ms.NAME = me.NAME;
